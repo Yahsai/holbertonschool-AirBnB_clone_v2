cb4679c313f6587c43278af433b6978ae6702c43
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-25 22:20:38 -0400

    updating dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,14 +3,14 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create user
-CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privileges
 GRANT
 SELECT
-    ON performance_schema.* TO 'hbnb_dev' @'localhost';
+    ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
