7e65e1ace37ac3febd5c70ae6d8f4f45989e59a3
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-25 22:15:36 -0400

    changing imports

--- models/state.py
+++ models/state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.city import City
+Base = declarative_base()
 
 
 class State(BaseModel, Base):
