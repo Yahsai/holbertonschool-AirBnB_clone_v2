8702997129f77e6a9e51448495c3292d3fb9c4d4
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-25 21:28:52 -0400

    updating files

---
+++ models/engine/db_storage.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import relationship, backref
+from models.base_model import Base
+from models.state import State
+from models.city import City
+
+
+class DBStorage:
+    """Class for DB Storage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Constructor"""
+        self.__engine = create_engine('mysql+mysqldb://HBNB_MYSQL_USER:HBNB_MYSQL_PWD@HBNB_MYSQL_HOST/HBNB_MYSQL_DB',
+                                      pool_pre_ping=True)
+
+    def all(self, cls=None):
+        """Query all objects"""
+        objects = {}
+        if cls:
+            query = self.__session.query(cls).all()
+            for obj in query:
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                objects[key] = obj
+        else:
+            for table in Base.metadata.tables.keys():
+                if table != 'states' and table != 'cities':
+                    continue
+                query = self.__session.query(eval(table.capitalize())).all()
+                for obj in query:
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    objects[key] = obj
+        return objects
+
+    def new(self, obj):
+        """Add the object to the current database session"""
+        if obj:
+            self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete from the current database session obj if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Reload objects from the database"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """Call remove() method on the private session attribute"""
+        self.__session.remove()
+
+    def get_cities(self, state_id):
+        """Get cities by state_id"""
+        state = self.__session.query(State).filter(State.id == state_id).first()
+        if state:
+            return state.cities
+        return []
--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan", backref="state")
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes State """
+        super().__init__(*args, **kwargs)
+
+    @property
+    def cities(self):
+        """ Getter attribute cities that returns a list of City instances
+            with state_id equals to the current State.id """
+        from models import storage
+        cities_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                cities_list.append(city)
+        return cities_list
