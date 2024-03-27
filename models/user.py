0077619d3118a221509c0841ebc3da45a27340d7
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-27 18:34:45 -0400

    adding import

--- models/user.py
+++ models/user.py
@@ -5,9 +5,10 @@ from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from models.base_model import Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     
     __tablename__ = "users"
