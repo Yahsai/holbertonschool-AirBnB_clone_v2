3c65214d6b7a0158ed3ac25f86a661ab933874fb
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-27 18:31:09 -0400

    updating state.py

--- models/state.py
+++ models/state.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 
+import models
+from models.city import City
+from os import getenv
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,14 +15,14 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
-    # Relación para DBStorage
-    cities = relationship("City", cascade="all, delete-orphan",
-                          backref="state")
-
-    # Getter para FileStorage
-    @property
-    def cities(self):
-        from models import storage
-        from models.city import City
-        return storage.all(City).filter(City.state_id == self.id)
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
