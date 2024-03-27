8de3d8ea8eb0a91e9c228c660a945e4a33ee08bc
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-27 18:05:53 -0400

    debugging

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
 from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
@@ -8,5 +9,5 @@ Base = declarative_base()
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__  = "states"
     name = Column(String(128), nullable=False)
