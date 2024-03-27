699d85de4aba3b566d723e865e406278907d4937
MMD <manuelmoralesdiaz0@gmail.com>
2024-03-27 18:18:16 -0400

    updating base model

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,6 @@ from models import storage
 
 Base = declarative_base()
 
-
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
@@ -19,8 +18,8 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             storage.new(self)
         else:
             for key, value in kwargs.items():
@@ -29,9 +28,9 @@ class BaseModel:
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
             if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
+                self.created_at = datetime.utcnow()
             if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
+                self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -40,20 +39,19 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = type(self).__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
-    
+
     def delete(self):
         """Delete the current instance from storage"""
         storage.delete(self)
