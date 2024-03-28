89d8aaa2d91e1985d746006cc06a4caf40d3c62d
Yahsai <yahsaisantana1@gmail.com>
2024-03-27 22:23:08 -0400

    Update user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """User class."""
@@ -10,6 +11,11 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         """Initialize User instance."""
